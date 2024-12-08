13e386d69eb9545666533d2d4d2c6f09f35c34e0
Marc-Bofarull <marc.bofarull@estudiantat.upc.edu>
2024-12-08 22:44:09 +0100

    Update Project.py
    Ara està net i he arreglat tots els problemes que tenia

--- Project.py
+++ Project.py
@@ -1,22 +1,20 @@
-
 import math
 import matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
 
-def fractal(l, n, angle): #per a que s'assembli mes a un triangle n ha de ser gran
-    x_dots = [-l]
-    y_up = [x_dots[0]*np.cos(angle)]
-    y_mid = [0]
-    y_down = [-x_dots[0]*np.cos(angle)]
+def fractal(l, n, angle): #per a que s'assembli mes a un triangle, n ha de ser gran
+    x_dots = []
+    y_up = []
+    y_mid = []
+    y_down = []
    
-    for i in range(1,n):
-        x_dots.append(-l/(i*2))
+    for i in range(n):
+        x_dots.append(l/(2**i))
         y_up.append(x_dots[i]*np.cos(angle))
         y_mid.append(0)
         y_down.append(-x_dots[i]*np.cos(angle))
-    # return (x_dots, y_dots, y_dots1, y_dots2)
-    # return x_dots
+        
     x_dots_combined = []
     y_dots_combined1 = []
     y_dots_combined2 = []
@@ -25,62 +23,34 @@ def fractal(l, n, angle): #per a que s'assembli mes a un triangle n ha de ser gr
         x_dots_combined.append(x_dots[i])
         x_dots_combined.append(x_dots[i])
 
-    for i in range(n-1):
+    for i in range(n):
+        y_dots_combined1.append(y_up[i])
         y_dots_combined1.append(0)
-        y_dots_combined1.append(y_up[i+1])
-    
-    for i in range(n-1):
-        y_dots_combined2.append(0)
-        y_dots_combined2.append(-y_up[i+1])
-        
-    x_dots_combined.pop(0)
-    #maybe if statement:
-    y_dots_combined1.append(0)
-  
-    y_dots_combined2.append(0)
 
     
+    for i in range(n):
+        y_dots_combined2.append(-y_up[i])
+        y_dots_combined2.append(0)
+
 
     plt.scatter(x_dots_combined, y_dots_combined1, marker = ".", s = 5, color = 'black')
-    plt.plot(x_dots_combined, y_dots_combined1, marker = ".", markersize = 2.5, linewidth = 0.5, color = 'black')    
+    plt.plot(x_dots_combined, y_dots_combined1, marker = ".", markersize = 0.5, linewidth = 0.5, color = 'black')   
+    
     plt.scatter(x_dots_combined, y_dots_combined2, marker = ".", s = 5, color = 'black')
-    plt.plot(x_dots_combined, y_dots_combined2, marker = ".", markersize = 2.5, linewidth = 0.5, color = 'black')
+    plt.plot(x_dots_combined, y_dots_combined2, marker = ".", markersize = 0.5, linewidth = 0.5, color = 'black')
 
-    # plt.scatter(x_dots, y_dots, marker = ".", s = 5)
     plt.scatter(x_dots, y_mid, marker = ".", s = 5, color = 'black')
-    # plt.scatter(x_dots, y_dots2, marker = ".", s = 5)
-    plt.plot(x_dots, y_up, marker = ".", markersize = 2.5, linewidth = 0.5, color = 'black')
-    # plt.plot(x_dots, y_dots1, marker = ".", markersize = 2.5)
-    plt.plot(x_dots, y_down, marker = ".", markersize = 2.5, linewidth = 0.5, color = 'black')
-        # plt.plot((x_dots[0], -y_dots2[0]), (x_dots[1], 0), linestyle = "-", marker = ".", markersize = 2.5, linewidth = 0.5)
+
+    plt.plot(x_dots, y_up, marker = ".", markersize = 0.5, linewidth = 0.5, color = 'black')
+
+    plt.plot(x_dots, y_down, marker = ".", markersize = 0.5, linewidth = 0.5, color = 'black')
+
 
     plt.vlines(x_dots, ymin = y_down, ymax = y_up, linewidth = 0.3, color = "black")
     
-    # for i in range(n):
-    # plt.fill_between(x_dots, y_up, y_down)
-    
-    plt.fill_between([-1, -0.5], [0, 0.25], [0, -0.25], color = 'black')
-    
-    for i in range(1,n+1):
-        plt.fill_between([-l/(i*2), -l/((i+1)*2)], [0,(x_dots[i+1]*np.cos(angle))], [0,-(x_dots[i+1]*np.cos(angle))], color = 'black')
-    
-    # plt.fill_between([-1, -0.5], [0, 0.25], [0, -0.25])
-    # plt.fill_between([-0.5, -0.25], [0, 0.125], [0, -0.125])
-    # plt.fill_between([-0.25, -0.1666666], [0, 0.08333333], [0, -0.0833333])
-    
-    # plt.fill_between(x_dots, y_dots_combined2, y_dots_combined1)
-    
+    for i in range(1,n):
+       plt.fill_between([l/(2**(i-1)), l/(2**(i))], [0,(x_dots[i]*np.cos(angle))], [0,-(x_dots[i]*np.cos(angle))], color = 'black')
+               
     plt.show()
     
-    return x_dots,y_up, y_down
-# (x_dots_combined,
-#         y_dots_combined1,
-#         y_dots_combined2, x_dots)
-
-# x = [-1,-0.5,-0.5,-0.25,-0.25,-0.125, -0.125]
-# y = [0,-0.5, 0,  -0.25, 0,   -0.125,  0]
-# plt.scatter(x, y, marker = ".", s = 5)
-# plt.plot(x, y, marker = ".", markersize = 2.5, linewidth = 0.5) 
-
 # if __name__=="__main__":
-
